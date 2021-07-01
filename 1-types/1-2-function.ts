2566e7b8721b5dccf9c0645ebab7dc50b2126e75
Min Jiyeon <jiyeon@Minui-MacBookAir.local>
2021-07-01 13:52:13 +0900

    Add : typescript study 3

--- 1-types/1-2-function.ts
+++ 1-types/1-2-function.ts
@@ -57,9 +57,18 @@
   // lastName? 는 lastName: string | undefined 란 뜻이다. 
   // Optional parameter 를 이용하면 인자를 전달해도 되고, 전달하지 않아도 된다.
 
-  // * 만약 Optional parameter를 이용하지 않고,
+  // * 비슷하지만 다른 방식도 있다.
+  // 만약 Optional parameter를 이용하지 않고,
   // lastName: string | undefined 라고 명시적으로 정의를 해놓으면,
-  // 항상 무조건 정확하게 undefined라고 정의를 해줘야 한다. (Optianal parameter를 사용하자)
+  function printNames(firstName: string, lastName: string | undefined) {
+    console.log(firstName);
+    console.log(lastName);
+  }
+  printNames('Steve', "dam");
+  printNames('min'); 
+  printNames('Anna', undefined);
+  // 항상 무조건 정확하게 undefined라고 정의를 해줘야 한다. (되도록이면, Optianal parameter를 사용하자)
+  
 
   // 2. Default parameter
   function printMessage(message : string) {
@@ -73,22 +82,22 @@
   function printMessages(message : string = 'default message') {
     console.log(message);
   }
-  printMessages();
+  printMessages(); // 아무런 인자를 전달하지 않았으나, default message인 'default message'가 출력된다.
+
   // 앞의 예시처럼 해당 함수에 아무런 인자를 전달하지 않았으나, 
   // Default parameter를 사용함으로써 error가 발생하지 않고,
   // 해당 Detault parameter 기본 값으로 지정해놓은 
   // 'default message' 값을 출력하는 것을 확인할 수 있다.
 
 
+
   // 3. Rest parameter
   // 만약 인자의 갯수의 제한 없이 원하는 만큼 전달하면
   // 알아서 자동으로 전부 더해주는 함수를 만들어야한다면 어떻게 해야할까?
-
   function addNumbers(...numbers: number[]): number {
     return numbers.reduce((a, b) => a+b);
   }
   console.log(addNumbers(1, 2));
   console.log(addNumbers(1, 2, 3, 4));
   console.log(addNumbers(1, 2, 3, 4, 5));
-
 }
