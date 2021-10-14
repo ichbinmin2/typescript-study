d360955a4649d0cb122cce2a8ca55de055f818a5
JiyeonMin <ichbinmin2@gmail.com>
2021-10-14 16:03:39 +0900

    typescript study-practice3

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -3,24 +3,53 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+
+  /** ellie solution */
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("👀 loading...");
+        break;
+      case "success":
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case "fail":
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state : ${state}`);
+    }
+  }
+
+  /** my A
+    function printLoginState(state: ResourceLoadState) {
+    if ("loading" in state) {
+      console.log("👀 loading...");
+    } else if ("success" in state) {
+      console.log("😃 loaded");
+    } else {
+      console.log("😱 no network");
+    }
+  }
+   */
 }
