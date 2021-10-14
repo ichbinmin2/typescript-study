2b6301bd1bf6b2b73940c48561c30c609e9660fb
JiyeonMin <ichbinmin2@gmail.com>
2021-10-14 17:00:15 +0900

    OOP study-1

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -41,6 +41,11 @@
     }
   }
 
+  /** switch VS if
+   * union type의 경우, 여러 케이스별로 다른 로직을 적용할 때는 switch 문이 더 적절하다.
+   * 만약, 다른 타입 등을 추가한다면 추가된 케이스는 전부 다 else 문 안에서 처리가 되기 때문에 예상치 못한 error가 발생할 확률이 높다.
+   */
+
   /** my A
     function printLoginState(state: ResourceLoadState) {
     if ("loading" in state) {
